6e2b6917df1bc326495551478e1a78dc9e937d77
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2018-01-30 16:21:43 +0900

    rev cell_relaxation for consistent output with vasprun's "Rakefile gets finished"

--- test/cell_relaxation.rb
+++ test/cell_relaxation.rb
@@ -3,17 +3,29 @@ require 'scanf'
 file = './POSCAR_0_3315_46_Al'
 model = EAM.new(file)
 
+aa = 19.8145937137
+bb = 10.3035887311
+cc = 4.04140
+n_atom = 46
+
 dx = 0.01
 dy = 0.01
 n = 5
-x0 = 1.0-((n-1)/2+1)*dx
-y0 = 1.0-((n-1)/2+1)*dy
+x0 = 1.0-((n-1)/2)*dx
+y0 = 1.0-((n-1)/2)*dy
+
 n.times do |i|
   n.times do |j|
     x = x0 + dx*i
     y = y0 + dy*j
-    printf("%5.2f, %5.2f, ", x-1, y-1)
+    printf("%4d %4d", (x-1)*100, (y-1)*100)
+    printf("%10.5f %10.5f %10.5f ", aa, bb, cc)
     model.set_cell_size(x,y)
-    printf("%10.5f\n",  model.total_energy)
+    df = model.total_energy
+    de = df - n_atom * (-3.39)
+    ss = aa*x * bb*y
+    es = de / ss * 1.60218 * 10 / 2
+    
+    printf("%-12.5f %-12.5f\n", df, es)
   end
 end
